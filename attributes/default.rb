e721abbcd9527a7193ca2e182b83423ad89676da
Mario Camou <mario.camou@yetu.com>
2014-11-07 17:03:33 +0100

    Rename the SmartOS version attribute to reflect that it is the version of the Riak package

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,3 @@
 default['riak']['release'] = '2.0'
 default['riak']['version'] = '2.0.2'
-default['smartos']['version'] = '13.1'
+default['riak']['smartos']['version'] = '13.1'
--- recipes/default.rb
+++ recipes/default.rb
@@ -5,11 +5,8 @@
 # Copyright (C) 2014 yetu AG
 #
 
-node.default['riak']['release'] = '2.0'
-node.default['riak']['version'] = '2.0.2'
-node.default['smartos']['version'] = '13.1'
 
 execute 'install riak from basho package' do
-  command "pkg_add http://s3.amazonaws.com/downloads.basho.com/riak/#{node.default['riak']['release']}/#{node.default['riak']['version']}/smartos/#{node.default['smartos']['version']}/riak-#{node.default['riak']['version']}-SmartOS-x86_64.tgz"
+  command "pkg_add http://s3.amazonaws.com/downloads.basho.com/riak/#{node.default['riak']['release']}/#{node.default['riak']['version']}/smartos/#{node.default['riak']['smartos']['version']}/riak-#{node.default['riak']['version']}-SmartOS-x86_64.tgz"
   not_if "pkg_info riak-#{node.default['riak']['version']}"
 end
